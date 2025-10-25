d15fa5803b5344d09d2bba48ec5235d41a1154a5
manoreken2 <manoreken2@gmail.com>
2025-10-25 20:26:38 +0900

    unsloth finetune_test

---
+++ fintune_test/README.md
@@ -0,0 +1,44 @@
+Setup
+
+requirements.txt is arranged for RTX5090.
+see the following page and install triton windows for your GPU
+https://github.com/woct0rdho/triton-windows
+
+
+
+install miniforge
+
+open miniforge prompt and type
+
+    # install ollama onto WSL Ubuntu Linux 24.04 
+    curl -fsSL https://ollama.com/install.sh | sh
+
+    # used by model.save_pretrained_gguf
+    sudo apt-get install libcurl4-openssl-dev cmake -y
+    
+    conda create -n Finetune python=3.12
+    conda activate Finetune
+
+    pip install -r requirements.txt
+
+
+    # https://developer.nvidia.com/cuda-downloads?target_os=Linux&target_arch=x86_64&Distribution=WSL-Ubuntu&target_version=2.0&target_type=deb_local
+    wget https://developer.download.nvidia.com/compute/cuda/repos/wsl-ubuntu/x86_64/cuda-wsl-ubuntu.pinsudo mv cuda-wsl-ubuntu.pin /etc/apt/preferences.d/cuda-repository-pin-600wget https://developer.download.nvidia.com/compute/cuda/13.0.2/local_installers/cuda-repo-wsl-ubuntu-13-0-local_13.0.2-1_amd64.debsudo dpkg -i cuda-repo-wsl-ubuntu-13-0-local_13.0.2-1_amd64.debsudo cp /var/cuda-repo-wsl-ubuntu-13-0-local/cuda-*-keyring.gpg /usr/share/keyrings/sudo apt-get updatesudo apt-get -y install cuda-toolkit-13-0
+
+    # PATHに/usr/local/cuda/binを追加
+
+    # First uninstall xformers installed by previous libraries
+    pip uninstall xformers -y
+
+    # install pytorch with 13.0
+    pip install torch==2.9.0+cu130 torchvision==0.24.0+cu130 --index-url https://download.pytorch.org/whl/cu130
+
+    # Clone and build xformers
+    pip install ninja
+    export TORCH_CUDA_ARCH_LIST="12.0"
+    git clone --depth=1 https://github.com/facebookresearch/xformers --recursive
+    cd xformers && python setup.py install && cd ..
+
+Llama3_8B_Ollama.ipynb is downloaded from
+https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Llama3_(8B)-Ollama.ipynb#scrollTo=MwEbRFl0Mf3E
+
---
+++ fintune_test/train.py
@@ -0,0 +1,248 @@
+# original file is https://colab.research.google.com/github/unslothai/notebooks/blob/main/nb/Llama3_(8B)-Ollama.ipynb
+
+from unsloth import FastLanguageModel
+import torch
+
+max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
+dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+
+# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
+fourbit_models = [
+    "unsloth/mistral-7b-v0.3-bnb-4bit",      # New Mistral v3 2x faster!
+    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
+    "unsloth/llama-3-8b-bnb-4bit",           # Llama-3 15 trillion tokens model 2x faster!
+    "unsloth/llama-3-8b-Instruct-bnb-4bit",
+    "unsloth/llama-3-70b-bnb-4bit",
+    "unsloth/Phi-3-mini-4k-instruct",        # Phi-3 2x faster!
+    "unsloth/Phi-3-medium-4k-instruct",
+    "unsloth/mistral-7b-bnb-4bit",
+    "unsloth/gemma-7b-bnb-4bit",             # Gemma 2.2x faster!
+] # More models at https://huggingface.co/unsloth
+
+model, tokenizer = FastLanguageModel.from_pretrained(
+    model_name = "unsloth/llama-3-8b-bnb-4bit",
+    max_seq_length = max_seq_length,
+    dtype = dtype,
+    load_in_4bit = load_in_4bit,
+    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
+)
+
+model = FastLanguageModel.get_peft_model(
+    model,
+    r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+    target_modules = ["q_proj", "k_proj", "v_proj", "o_proj",
+                      "gate_proj", "up_proj", "down_proj",],
+    lora_alpha = 16,
+    lora_dropout = 0, # Supports any, but = 0 is optimized
+    bias = "none",    # Supports any, but = "none" is optimized
+    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
+    use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
+    random_state = 3407,
+    use_rslora = False,  # We support rank stabilized LoRA
+    loftq_config = None, # And LoftQ
+)
+
+from datasets import load_dataset
+
+dataset = load_dataset("vicgalle/alpaca-gpt4", split="train")
+print(dataset.column_names)
+
+# datasetは、~/.cache/huggingface/datasets/vicgalle___alpaca-gpt4に保存される。
+
+
+# instruction, input, output, textの4項目がある。
+print(dataset.column_names)
+
+# instructionとinputをつなげてmerged_promptを作成。
+from unsloth import to_sharegpt
+
+dataset = to_sharegpt(
+    dataset,
+    merged_prompt="{instruction}[[\nYour input is:\n{input}]]",
+    output_column_name="output",
+    conversation_extension=3,  # Select more to handle longer conversations
+)
+
+# fixup the dataset.
+from unsloth import standardize_sharegpt
+
+dataset = standardize_sharegpt(dataset)
+
+
+alpaca_prompt = """Below is an instruction that describes a task, paired with an input that provides further context. Write a response that appropriately completes the request.
+
+### Instruction:
+{}
+
+### Input:
+{}
+
+### Response:
+{}"""
+
+chat_template = """Below are some instructions that describe some tasks. Write responses that appropriately complete each request.
+
+### Instruction:
+{INPUT}
+
+### Response:
+{OUTPUT}"""
+
+from unsloth import apply_chat_template
+
+dataset = apply_chat_template(
+    dataset,
+    tokenizer=tokenizer,
+    chat_template=chat_template,
+    # default_system_message = "You are a helpful assistant", << [OPTIONAL]
+)
+
+
+# train the model
+from trl import SFTConfig, SFTTrainer
+trainer = SFTTrainer(
+    model = model,
+    tokenizer = tokenizer,
+    train_dataset = dataset,
+    dataset_text_field = "text",
+    max_seq_length = max_seq_length,
+    packing = False, # Can make training 5x faster for short sequences.
+    args = SFTConfig(
+        per_device_train_batch_size = 2,
+        gradient_accumulation_steps = 4,
+        warmup_steps = 5,
+        max_steps = 60,
+        # num_train_epochs = 1, # For longer training runs!
+        learning_rate = 2e-4,
+        logging_steps = 1,
+        optim = "adamw_8bit",
+        weight_decay = 0.01,
+        lr_scheduler_type = "linear",
+        seed = 3407,
+        output_dir = "outputs",
+        report_to = "none", # Use TrackIO/WandB etc
+    ),
+)
+
+# @title Show current memory stats
+gpu_stats = torch.cuda.get_device_properties(0)
+start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
+print(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
+print(f"{start_gpu_memory} GB of memory reserved.")
+
+trainer_stats = trainer.train()
+
+# @title Show final memory and time stats
+used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
+used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
+used_percentage = round(used_memory / max_memory * 100, 3)
+lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
+print(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
+print(
+    f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
+)
+print(f"Peak reserved memory = {used_memory} GB.")
+print(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
+print(f"Peak reserved memory % of max memory = {used_percentage} %.")
+print(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
+
+
+FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+messages = [                    # Change below!
+    {"role": "user", "content": "Continue the fibonacci sequence! Your input is 1, 1, 2, 3, 5, 8,"},
+]
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt = True,
+    return_tensors = "pt",
+).to("cuda")
+
+from transformers import TextStreamer
+
+text_streamer = TextStreamer(tokenizer, skip_prompt = True)
+_ = model.generate(input_ids, streamer = text_streamer, max_new_tokens = 128, pad_token_id = tokenizer.eos_token_id)
+
+
+FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+messages = [                         # Change below!
+    {"role": "user",      "content": "Continue the fibonacci sequence! Your input is 1, 1, 2, 3, 5, 8"},
+    {"role": "assistant", "content": "The fibonacci sequence continues as 13, 21, 34, 55 and 89."},
+    {"role": "user",      "content": "What is France's tallest tower called?"},
+]
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt = True,
+    return_tensors = "pt",
+).to("cuda")
+
+from transformers import TextStreamer
+
+text_streamer = TextStreamer(tokenizer, skip_prompt = True)
+_ = model.generate(input_ids, streamer = text_streamer, max_new_tokens = 128, pad_token_id = tokenizer.eos_token_id)
+
+
+# save the trained model
+
+model.save_pretrained("lora_model")  # Local saving
+tokenizer.save_pretrained("lora_model")
+# model.push_to_hub("your_name/lora_model", token = "...") # Online saving
+# tokenizer.push_to_hub("your_name/lora_model", token = "...") # Online saving
+
+if False:
+    from unsloth import FastLanguageModel
+    model, tokenizer = FastLanguageModel.from_pretrained(
+        model_name = "lora_model", # YOUR MODEL YOU USED FOR TRAINING
+        max_seq_length = max_seq_length,
+        dtype = dtype,
+        load_in_4bit = load_in_4bit,
+    )
+    FastLanguageModel.for_inference(model) # Enable native 2x faster inference
+
+messages = [                    # Change below!
+    {"role": "user", "content": "Describe anything special about a sequence. Your input is 1, 1, 2, 3, 5, 8,"},
+]
+input_ids = tokenizer.apply_chat_template(
+    messages,
+    add_generation_prompt = True,
+    return_tensors = "pt",
+).to("cuda")
+
+from transformers import TextStreamer
+text_streamer = TextStreamer(tokenizer, skip_prompt = True)
+_ = model.generate(input_ids, streamer = text_streamer, max_new_tokens = 128, pad_token_id = tokenizer.eos_token_id)
+
+
+if False:
+    # I highly do NOT suggest - use Unsloth if possible
+    from peft import AutoPeftModelForCausalLM
+    from transformers import AutoTokenizer
+    model = AutoPeftModelForCausalLM.from_pretrained(
+        "lora_model", # YOUR MODEL YOU USED FOR TRAINING
+        load_in_4bit = load_in_4bit,
+    )
+    tokenizer = AutoTokenizer.from_pretrained("lora_model")
+    
+# Save to q4_k_m GGUF
+if True: model.save_pretrained_gguf("model", tokenizer, quantization_method = "q4_k_m")
+
+# Remember to go to https://huggingface.co/settings/tokens for a token!
+# And change hf to your username!
+if False: model.push_to_hub_gguf("hf/model", tokenizer, quantization_method = "q4_k_m", token = "")
+
+# run ollama
+import subprocess
+
+subprocess.Popen(["ollama", "serve"])
+import time
+
+time.sleep(3)  # Wait for a few seconds for Ollama to load!
+
+print(tokenizer._ollama_modelfile)
+
+print("Copy-paste the Modelfile text above and create Modelfile.")
+print("Type the following command to the shell to create finetuned model")
+print("ollama create unsloth_model -f ./model/Modelfile")
+
+

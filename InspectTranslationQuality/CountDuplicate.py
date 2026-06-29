f98a9027cc350bfd1f6d79dcb48edd4fa4faf7e5
manoreken <manoreken2@gmail.com>
2026-06-30 07:10:06 +0900

    太平記 日本語訳

--- InspectTranslationQuality/CountDuplicate.py
+++ InspectTranslationQuality/CountDuplicate.py
@@ -20,14 +20,16 @@ def run(args):
             trans_cnt += 1
 
             translated_txt = lines[i + 1]
-            txt_bgn = translated_txt[: args.compare_char_count]
+            txt_bgn = translated_txt[: args.inspect_len]
+            if args.print_all_translation_beginning:
+                print(f"      {txt_bgn}")
             if prev_txt_bgn == txt_bgn:
                 print(f"    line {i+1} : possible duplicate {txt_bgn}")
                 dup += 1
             prev_txt_bgn = txt_bgn
 
     print(
-        f'"{args.input_html_path}" translation_coun {trans_cnt}, possible_duplicate {dup}'
+        f'"{args.input_html_path}" translation_count {trans_cnt}, possible_duplicate {dup}'
     )
 
 
@@ -35,8 +37,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input_html_path", type=str, help="input html path")
     parser.add_argument(
-        "--compare_char_count", type=int, default=25, help="compare characters count"
+        "--inspect_len", type=int, default=25, help="compare characters count"
     )
+    parser.add_argument("--print_all_translation_beginning", type=bool, default=False)
     args = parser.parse_args()
 
     run(args)
--- InspectTranslationQuality/run.bat
+++ InspectTranslationQuality/run.bat
@@ -1,3 +1,3 @@
-python CountDuplicate.py "../translate_dir_files_with_context/史記_日本語訳.html"
-python CountDuplicate.py "../translate_dir_files_with_context/太平記_日本語訳.html"
+python CountDuplicate.py "../translate_dir_files_with_context/史記_日本語訳.html"   --inspect_len=40 --print_all_translation_beginning=True
+python CountDuplicate.py "../translate_dir_files_with_context/太平記_日本語訳.html" --inspect_len=40 --print_all_translation_beginning=True
 
